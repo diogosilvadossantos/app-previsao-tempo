28e43ae2ccd7db8da2cd921c8ea9f6855c4516ec
Diogo dos Santos <diogosilvasantos_31@hotmail.com>
2024-05-30 18:39:00 -0300

    finalização versão 0.0.1

--- scripts.js
+++ scripts.js
@@ -1,7 +1,37 @@
-// const key = "f6a85c8d8fe0c9dfa2ccad3265c77cdb";
+const key = "f6a85c8d8fe0c9dfa2ccad3265c77cdb";
+
+function displayCity(data) {
+  document.querySelector(".city").innerHTML =
+    "Tempo em " + data.name + ", " + data.sys.country;
+  document.querySelector(".temp").innerHTML =
+    "Temperatura: " + Math.floor(data.main.temp) + "°C";
+  document.querySelector(".temp-min").innerHTML =
+    "Mínima: " + Math.floor(data.main.temp_min) + "°C";
+  document.querySelector(".temp-max").innerHTML =
+    "Máxima: " + Math.floor(data.main.temp_max) + "°C";
+  document.querySelector(".text-forecast").innerHTML =
+    data.weather[0].description;
+  document.querySelector(".text-humidity").innerHTML =
+    "Umidade: " + data.main.humidity + "%";
+  document.querySelector(
+    ".img-temp"
+  ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+}
+
+async function citySearch(city) {
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+  ).then((response) => response.json());
+
+  displayCity(data);
+
+  console.log(data);
+}
 
 function clickSearch() {
-  const input = document.querySelector(".input-city").value;
+  const city = document.querySelector(".input-city").value;
 
-  console.log(input);
+  citySearch(city);
+
+  document.querySelector(".input-city").value = "";
 }
